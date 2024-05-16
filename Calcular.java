5d7551ac3668947ea57b445d719dee82cfd8557e
Pedro Lima <96852030+PedroHLcordeiro@users.noreply.github.com>
2024-05-15 22:10:38 -0300

    Create Calcular.java

---
+++ Calcular.java
@@ -0,0 +1,17 @@
+import javax.sql.rowset.CachedRowSet;
+
+public class Calcular {
+    private double variação;
+
+    public void balança(Conta conta) {
+        if (preço > i);
+        preço == Caro;
+             else (preço < i);
+             preço == Barato;
+    }
+
+    public double getPrecoTotal() {
+        return precoTotal;
+    }
+}
+
