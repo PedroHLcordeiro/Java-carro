643b812be9641c938c6b77e5fa22095557f79dd6
Pedro Lima <96852030+PedroHLcordeiro@users.noreply.github.com>
2024-05-15 22:11:05 -0300

    Create Ficha.java

---
+++ Ficha.java
@@ -0,0 +1,17 @@
+import javax.sql.rowset.CachedRowSet;
+
+public class Cacular {
+    private double variação;
+
+    public void balança(Conta conta) {
+        if (preço > i);
+        preço == Caro;
+             else (preço < i);
+             preço == Barato;
+    }
+
+    public double getPrecoTotal() {
+        return precoTotal;
+    }
+}
+
